4ea3ff04ee18c40a0503620ff4718decb3a9d94b
17788766689 <17788766689@163.com>
2020-04-12 15:39:15 +0800

    宅仔修护

--- app/src/main/java/com/cainiao/action/ZZAction.java
+++ app/src/main/java/com/cainiao/action/ZZAction.java
@@ -74,49 +74,77 @@ public class ZZAction extends BaseAction {
                 .execute(new StringCallback() {
                     @Override
                     public void onSuccess(Response<String> response) {
-                        token = response.body().toString().substring(response.body().indexOf("btwaf=")+6,response.body().indexOf("btwaf=")+14);
-                        System.out.println(token+"-------");
-                        HttpClient.getInstance().post("/iop/register/loginActApp?btwaf="+Integer.valueOf(token), mPlatform.getHost())
-                                .params("moblie", mParams.getAccount())
-                                .params("password", mParams.getPassword())
-                                .params("devicename","" )
-                                .params("deviceid","" )
-                                .headers("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Mobile Safari/537.36")
-                                .headers("Origin", "http://xk.51zugeju.com")
-                                .headers("Proxy-Connection", "keep-alive")
-                                .headers("Referer", "http://xk.51zugeju.com/iop/web/logionapp.html?btwaf="+Integer.valueOf(token))
-                                .headers("X-Requested-With", "XMLHttpRequest")
-                                .execute(new StringCallback() {
-                                    @Override
-                                    public void onSuccess(Response<String> response) {
-                                        try {
-                                            if (TextUtils.isEmpty(response.body())) return;
-                                            if(response.body().indexOf("频繁") != -1){
-                                                sendLog("请求过于频繁,请几分钟后重试");
-                                                return;
-                                            }
-                                            JSONObject jsonObject = JSONObject.parseObject(response.body());
-                                            if ("登录成功".equals(jsonObject.getString("msg"))) {    //登录成功
-                                                List<String> cookies = response.headers().values("Set-Cookie");
-                                                for (String str : cookies) {
-                                                    cookie += str.substring(0, str.indexOf(";")) + ";";
+                        if (response.body().indexOf("btwaf") != -1){
+                            token = response.body().toString().substring(response.body().indexOf("btwaf=")+6,response.body().indexOf("btwaf=")+14);
+                            HttpClient.getInstance().post("/iop/register/loginActApp?btwaf="+token, mPlatform.getHost())
+                                    .params("moblie", mParams.getAccount())
+                                    .params("password", mParams.getPassword())
+                                    .params("devicename","" )
+                                    .params("deviceid","" )
+                                    .headers("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Mobile Safari/537.36")
+                                    .headers("Origin", "http://xk.51zugeju.com")
+                                    .headers("Proxy-Connection", "keep-alive")
+                                    .headers("Referer", "http://xk.51zugeju.com/iop/web/logionapp.html")
+                                    .headers("X-Requested-With", "XMLHttpRequest")
+                                    .execute(new StringCallback() {
+                                        @Override
+                                        public void onSuccess(Response<String> response) {
+                                            try {
+                                                if (TextUtils.isEmpty(response.body())) return;
+                                                if(response.body().indexOf("频繁") != -1){
+                                                    sendLog("请求过于频繁,请几分钟后重试");
+                                                    return;
                                                 }
-                                                sendLog("登录成功");
-                                                updateParams(mPlatform);
-                                                MyToast.info(MyApp.getContext().getString(R.string.receipt_start));
-                                                updateStatus(mPlatform, 3); //正在接单的状态
-                                                startTask();
-                                            } else {
-                                                MyToast.error(jsonObject.getString("msg"));
-                                                sendLog(jsonObject.getString("msg"));
+                                                JSONObject jsonObject = JSONObject.parseObject(response.body());
+                                                if ("登录成功".equals(jsonObject.getString("msg"))) {    //登录成功
+                                                    List<String> cookies = response.headers().values("Set-Cookie");
+                                                    for (String str : cookies) {
+                                                        cookie += str.substring(0, str.indexOf(";")) + ";";
+                                                    }
+                                                    sendLog("登录成功");
+                                                    updateParams(mPlatform);
+                                                    MyToast.info(MyApp.getContext().getString(R.string.receipt_start));
+                                                    updateStatus(mPlatform, 3); //正在接单的状态
+                                                    startTask();
+                                                } else {
+                                                    MyToast.error(jsonObject.getString("msg"));
+                                                    sendLog(jsonObject.getString("msg"));
+                                                    stop();
+                                                }
+                                            } catch (Exception e) {
+                                                sendLog("登录异常！");
                                                 stop();
                                             }
-                                        } catch (Exception e) {
-                                            sendLog("登录异常！");
-                                            stop();
                                         }
+                                    });
+                        }else {
+                            try {
+                                if (TextUtils.isEmpty(response.body())) return;
+                                if(response.body().indexOf("频繁") != -1){
+                                    sendLog("请求过于频繁,请几分钟后重试");
+                                    return;
+                                }
+                                JSONObject jsonObject = JSONObject.parseObject(response.body());
+                                if ("登录成功".equals(jsonObject.getString("msg"))) {    //登录成功
+                                    List<String> cookies = response.headers().values("Set-Cookie");
+                                    for (String str : cookies) {
+                                        cookie += str.substring(0, str.indexOf(";")) + ";";
                                     }
-                                });
+                                    sendLog("登录成功");
+                                    updateParams(mPlatform);
+                                    MyToast.info(MyApp.getContext().getString(R.string.receipt_start));
+                                    updateStatus(mPlatform, 3); //正在接单的状态
+                                    startTask();
+                                } else {
+                                    MyToast.error(jsonObject.getString("msg"));
+                                    sendLog(jsonObject.getString("msg"));
+                                    stop();
+                                }
+                            } catch (Exception e) {
+                                sendLog("登录异常！");
+                                stop();
+                            }
+                        }
                     }
                 });
     }
@@ -127,29 +155,60 @@ public class ZZAction extends BaseAction {
     private void startTask() {
         if(isStart == false)return;
         HttpClient.getInstance().get("/iop/index/autoindex?type="+mParams.getType() , mPlatform.getHost())
-                .headers("User-Agent", "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.186 Mobile Safari/537.36 Html5Plus/1.0")
-                .headers("Referer", "http://rengwu.yueban03.top/iop/index/index")
+                .headers("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Mobile Safari/537.36")
+                .headers("Referer", "http://xk.51zugeju.com/iop/index/index")
                 .headers("X-Requested-With", "XMLHttpRequest")
                 .headers("Cookie", cookie)
                 .execute(new StringCallback() {
                     @Override
                     public void onSuccess(Response<String> response) {
-                        try {
-                            if (TextUtils.isEmpty(response.body())) {
-                                sendLog("继续检测任务");
-                            } else {
-                                JSONObject obj = JSONObject.parseObject(response.body());
-                                sendLog(obj.getString("msg"));
-                                if ("1".equals(obj.getString("status"))) {
-                                    sendLog(MyApp.getContext().getString(R.string.KSHG_AW));
-                                    receiveSuccess(String.format(MyApp.getContext().getString(R.string.KSHG_AW_tips), mPlatform.getName()), R.raw.zhaizai, 3000);
-                                    addTask(mPlatform.getName());
-                                    updateStatus(mPlatform, Const.KSHG_AW); //接单成功的状态
-                                    isStart = false;
+                        if (response.body().indexOf("btwaf") != -1){
+                            token = response.body().toString().substring(response.body().indexOf("btwaf=")+6,response.body().indexOf("btwaf=")+14);
+                            HttpClient.getInstance().post("/iop/index/autoindex?type="+mParams.getType()+"&btwaf="+Integer.valueOf(token), mPlatform.getHost())
+                                    .headers("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Mobile Safari/537.36")
+                                    .headers("Referer", "http://xk.51zugeju.com/iop/index/index")
+                                    .headers("X-Requested-With", "XMLHttpRequest")
+                                    .headers("Cookie", cookie)
+                                    .execute(new StringCallback() {
+                                        @Override
+                                        public void onSuccess(Response<String> response) {
+                                            try {
+                                                if (TextUtils.isEmpty(response.body())) {
+                                                    sendLog("继续检测任务");
+                                                } else {
+                                                    JSONObject obj = JSONObject.parseObject(response.body());
+                                                    sendLog(obj.getString("msg"));
+                                                    if ("1".equals(obj.getString("status"))) {
+                                                        sendLog(MyApp.getContext().getString(R.string.KSHG_AW));
+                                                        receiveSuccess(String.format(MyApp.getContext().getString(R.string.KSHG_AW_tips), mPlatform.getName()), R.raw.zhaizai, 3000);
+                                                        addTask(mPlatform.getName());
+                                                        updateStatus(mPlatform, Const.KSHG_AW); //接单成功的状态
+                                                        isStart = false;
+                                                    }
+                                                }
+                                            } catch (Exception e) {
+                                                sendLog("检测任务异常！");
+                                            }
+                                        }
+                                    });
+                        }else {
+                            try {
+                                if (TextUtils.isEmpty(response.body())) {
+                                    sendLog("继续检测任务");
+                                } else {
+                                    JSONObject obj = JSONObject.parseObject(response.body());
+                                    sendLog(obj.getString("msg"));
+                                    if ("1".equals(obj.getString("status"))) {
+                                        sendLog(MyApp.getContext().getString(R.string.KSHG_AW));
+                                        receiveSuccess(String.format(MyApp.getContext().getString(R.string.KSHG_AW_tips), mPlatform.getName()), R.raw.zhaizai, 3000);
+                                        addTask(mPlatform.getName());
+                                        updateStatus(mPlatform, Const.KSHG_AW); //接单成功的状态
+                                        isStart = false;
+                                    }
                                 }
+                            } catch (Exception e) {
+                                sendLog(""+response.body());
                             }
-                        } catch (Exception e) {
-                            sendLog("检测任务异常！");
                         }
                     }
 
